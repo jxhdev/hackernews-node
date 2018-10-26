ea2ef7e652ce69691092d7c7d798033394a643b4
jxhdev <jxhdev@gmail.com>
2018-10-25 21:26:42 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { Prisma } = require('prisma-binding');
+const { GraphQLServer } = require('graphql-yoga');
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API of a Hackernews Clone!',
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    }
+  },
+  Mutation: {
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      );
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/jason-hu-a4d479/database/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
+});
+
+server.start(() => console.log('Server is running on http://localhost:4000'));
